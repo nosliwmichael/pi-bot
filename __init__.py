521120a9b01d6adf61478c25789e4953f2d6b690
Michael Wilson <nosliw.michael@gmail.com>
2020-09-01 21:40:51 -0500

    Update __init__.py
    Updating GPIO pin selection

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ import json
 
 with open('./keyMap.json') as keyMap_file:
     data = json.load(keyMap_file)
-    motorEvents = MotorEvents(left_pins=[5, 6], right_pins=[13, 19])
+    motorEvents = MotorEvents(left_pins=[11, 13], right_pins=[16, 18])
     keyMap = KeyMap(data, motorEvents)
     keyboard = KeyboardListener(keyMap)
     keyboard.start()
