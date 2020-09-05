af296a22dca3d2729e1c2d932e1ed305565757fe
Michael Wilson <nosliw.michael@gmail.com>
2020-09-05 16:51:54 -0500

    Moving python files to pi-bot-controller directory

---
+++ pi-bot-controller/dev-utilities/JSON_builder.py
@@ -0,0 +1,44 @@
+import json
+
+## BUILD JSON FILE ##
+data = {}
+data['inputs'] = [
+    {
+        'name': 'up',
+        'pressed': 'up_event',
+        'released': 'stop_event'
+    },
+    {
+        'name': 'down',
+        'pressed': 'down_event',
+        'released': 'stop_event'
+    },
+    {
+        'name': 'left',
+        'pressed': 'left_event',
+        'released': 'stop_event'
+    },
+    {
+        'name': 'right',
+        'pressed': 'right_event',
+        'released': 'stop_event'
+    },
+    {
+        'name': 'space',
+        'pressed': 'stop_event',
+        'released': None
+    },
+    {
+        'name': 'esc',
+        'pressed': 'close_event',
+        'released': None
+    }
+]
+data['pins'] = {
+    'left_forward': 27,
+    'left_backward': 17,
+    'right_forward': 6,
+    'right_backward': 23
+}
+with open('./data-map.json', 'w') as outfile:
+    json.dump(data, outfile)
