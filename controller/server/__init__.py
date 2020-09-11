d27179f2a87efdda17074d3b87bc3a9c759e6f4e
Michael Wilson <nosliw.michael@gmail.com>
2020-09-11 16:47:16 -0500

    Testing flask web page.

--- controller/server/__init__.py
+++ controller/server/__init__.py
@@ -1,34 +1,38 @@
 #!python3
-import json
-from motor_events import MotorEvents
-from flask import Flask, Response, request, make_response, jsonify
+# import json
+# from motor_events import MotorEvents
+import flask
 from flask_cors import CORS
 import pi_camera
 
-with open('./data-map.json') as data_map_file:
+# with open('./data-map.json') as data_map_file:
 
-    # Load JSON as dict
-    data_map = json.load(data_map_file)
+#     # Load JSON as dict
+#     data_map = json.load(data_map_file)
     
-    # Create motor object. Pass in GPIO pins that are used for wheels
-    motor_events = MotorEvents(data_map['pins'])
+#     # Create motor object. Pass in GPIO pins that are used for wheels
+#     motor_events = MotorEvents(data_map['pins'])
 
 # Create flask server application
-app = Flask(__name__)
+app = flask.Flask(__name__)
 CORS(app)
 
 # Define a route
-@app.route(rule='/control', methods=['POST'])
-def control():
-    body = request.get_json()
-    event_name = body['event']
-    event_method = getattr(motor_events, event_name)
-    event_response = event_method()
-    return jsonify(message = event_response if event_response != None else 'Command already in progress..')
+# @app.route(rule='/control', methods=['POST'])
+# def control():
+#     body = flask.request.get_json()
+#     event_name = body['event']
+#     event_method = getattr(motor_events, event_name)
+#     event_response = event_method()
+#     return flask.jsonify(message = event_response if event_response != None else 'Command already in progress..')
 
-@app.route(rule='/stream', methods=['GET'])
+@app.route(rule='/pi-cam', methods=['GET'])
+def web_stream():
+    return flask.render_template('web/index.html')
+
+@app.route(rule='/video-stream', methods=['GET'])
 def stream():
-    resp = Response(camera.stream_generator())
+    resp = flask.Response(camera.stream_generator())
     resp.headers["Access-Control-Allow-Origin"] = "*"
     resp.headers["Age"] = 0
     resp.headers["Cache-Control"] = "no-cache, private"
